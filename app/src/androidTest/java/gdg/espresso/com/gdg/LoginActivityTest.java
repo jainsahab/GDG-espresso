92ef283b48f11570f8f10e9b2b7bd705b9a80f2d
Prateek Jain <prateek.jain23995@yahoo.com>
2017-07-30 11:18:26 +0530

    creating custom matcher to verify the text color

--- app/src/androidTest/java/gdg/espresso/com/gdg/LoginActivityTest.java
+++ app/src/androidTest/java/gdg/espresso/com/gdg/LoginActivityTest.java
@@ -2,8 +2,13 @@ package gdg.espresso.com.gdg;
 
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import android.widget.TextView;
 
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -29,6 +34,7 @@ public class LoginActivityTest {
         onView(withId(R.id.email_sign_in_button)).perform(click());
 
         onView(withId(R.id.login_result)).check(matches(withText("Login Successful")));
+        onView(withId(R.id.login_result)).check(matches(withTextColor(android.R.color.holo_green_dark)));
     }
 
     @Test
@@ -41,5 +47,20 @@ public class LoginActivityTest {
         onView(withId(R.id.email_sign_in_button)).perform(click());
 
         onView(withId(R.id.login_result)).check(matches(withText("Login Failed")));
+        onView(withId(R.id.login_result)).check(matches(withTextColor(android.R.color.holo_red_dark)));
+    }
+
+    private Matcher<? super View> withTextColor(final int colorResourceId) {
+        return new CustomTypeSafeMatcher<View>("color with" + colorResourceId) {
+            @Override
+            protected boolean matchesSafely(View view) {
+                if (!(view instanceof TextView)) {
+                    return false;
+                }
+                TextView textView = (TextView) view;
+                ColorStateList expectedColorStateList = ColorStateList.valueOf(activityTestRule.getActivity().getResources().getColor(colorResourceId));
+                return textView.getTextColors().getDefaultColor() == expectedColorStateList.getDefaultColor();
+            }
+        };
     }
 }
